09e22a87c522f417b96647513fb4977ace216aa0
Diego Guedes <diego.braga.guedes@outlook.com>
2023-04-22 02:05:46 -0300

    fix

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -328,10 +328,6 @@ async function downloadBuild(filename, gameName, onProgress) {
     (err) => { }
   );
 
-  console.log(`https://launcher.golden-helmet.tk/${gameName}/${filename
-  .split(/[\\/]/)
-  .pop()}`);
-
   await downloadWithProgress(
     `https://launcher.golden-helmet.tk/${gameName}/${filename
       .split(/[\\/]/)
